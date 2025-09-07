225120b68f90943149d3c7b67b1b5f13a8dafc04
karanpraja902 <karanpraja902@gmail.com>
2025-09-07 13:53:37 +0530

    Update server configuration to set default PORT to 5000 and log the running environment mode on startup

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-const PORT = process.env.NODE_ENV|| process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 
 // api's
@@ -48,4 +48,5 @@ app.listen(PORT,(req,res)=>{
     res.send("Server is running");
     connectDB();
     console.log(`Server running at port ${PORT}`);
+    console.log(`Server running in ${process.env.NODE_ENV} mode`);
 })
